17696aec5fd450c15a8f9481e041faa0946b3e7f
Matheus <matheus.etb@gmail.com>
2020-09-12 17:11:07 -0300

    Estrutura inicial do ms de tickets

---
+++ src/test/java/com/eventosdahora/ticket/ms/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.eventosdahora.ticket.ms;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
